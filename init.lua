cd0cbd7edf736a1546c417e6f659da2558b8a562
Carl Bäckström <genzorg@gmail.com>
2019-09-19 11:55:18 +0200

    Reformat code with PyCharm

--- init.lua
+++ init.lua
@@ -19,8 +19,8 @@ hs.window.animationDuration = 0 -- disable animations
 -- Higher is more verbose
 hs.logger.setGlobalLogLevel(5)
 
-function string.starts(String,Start)
-   return string.sub(String,1,string.len(Start))==Start
+function string.starts(String, Start)
+    return string.sub(String, 1, string.len(Start)) == Start
 end
 
 local function getFilteredWindowLayout (windowLayout, windowTitle)
@@ -50,7 +50,6 @@ function hideAllActiveWindows()
     end
 end
 
-
 function showWindowsInAllWindows(AllWindows)
     for index, window in ipairs(AllWindows) do
         window:unminimize()
@@ -66,69 +65,69 @@ end
 function getAllWindowsAsWindowLayout(window)
     newWindowLayout = {}
     for index, window in ipairs(hs.window.visibleWindows()) do
-        table.insert(newWindowLayout, {window:application(), window, window:screen(), nil, window:frame(), nil })
+        table.insert(newWindowLayout, { window:application(), window, window:screen(), nil, window:frame(), nil })
     end
     return newWindowLayout
 end
 
-
 function arrangeWindows(windowTitle)
-  local windowLayout = {}
-  local lowerRight = hs.geometry.unitrect({x=0.5, y=0.5, w=0.5, h=0.5})
-  local upperRight = hs.geometry.unitrect({x=0.5, y=0, w=0.5, h=0.5})
-  local upper50RightLeft = hs.geometry.unitrect({x=0.5, y=0, w=0.25, h=0.5})
-  local upper50RightRight = hs.geometry.unitrect({x=0.75, y=0, w=0.25, h=0.5})
+    local windowLayout = {}
+    local lowerRight = hs.geometry.unitrect({ x = 0.5, y = 0.5, w = 0.5, h = 0.5 })
+    local upperRight = hs.geometry.unitrect({ x = 0.5, y = 0, w = 0.5, h = 0.5 })
+    local upper50RightLeft = hs.geometry.unitrect({ x = 0.5, y = 0, w = 0.25, h = 0.5 })
+    local upper50RightRight = hs.geometry.unitrect({ x = 0.75, y = 0, w = 0.25, h = 0.5 })
 
-
-  if hs.screen.allScreens()[1]:name() == "PHL BDM4037U" then
-    monitor1 = hs.screen.allScreens()[1]:name()
-    -- maximized window hs.geometry.unitrect({x=1, y=1, w=1, h=1}).
-    windowLayout = {
-        {"Google Chrome",  nil, monitor1, hs.geometry.unitrect({x=0.5, y=0, w=0.25, h=1}), nil, nil},
-        {"Firefox",  nil, monitor1, hs.geometry.unitrect({x=0.5, y=0, w=0.25, h=1}), nil, nil},
-        {"PyCharm",  nil, monitor1, hs.layout.left50, nil, nil},
-        {"WebStorm",  nil, monitor1, hs.layout.left50, nil, nil},
-        {"PyCharm",  "Commit Changes", monitor1, hs.layout.right50, nil, nil},
-        {"Sublime Text",  nil, monitor1, upper50RightRight, nil},
-        {"Mail",  nil, monitor1, upper50RightRight, nil, nil},
-        {"Microsoft Teams",  nil, monitor1, upper50RightRight, nil, nil},
-        {"Skype for Business",  nil, monitor1, upper50RightRight, nil, nil},
-        {"Franz",  nil, monitor2, upper50RightRight, nil, nil},
-        {"Spotify",  nil, monitor1, lowerRight, nil, nil},
-        {"Todoist",  nil, monitor1, lowerRight, nil, nil},
-        {"Iterm2",  nil, monitor1, upper50RightRight, nil, nil},
-        {"Calculator",  nil, monitor1, lowerRight, nil, nil},
-    }
-  else
-    monitor1 = hs.screen.allScreens()[1]:name()
-    monitor2 = hs.screen.allScreens()[2]:name()
-    -- maximized window hs.geometry.unitrect({x=1, y=1, w=1, h=1}).
-    windowLayout = {
-        {"Google Chrome",  nil, monitor2, hs.layout.left50, nil, nil},
-        {"Firefox",  nil, monitor2, hs.layout.left50, nil, nil},
-        {"PyCharm",  nil, monitor1, hs.layout.left50, nil, nil},
-        {"PyCharm",  "Commit Changes", monitor1, hs.layout.right50, nil, nil},
-        {"WebStorm",  nil, monitor1, hs.layout.left50, nil, nil},
-        {"Sublime Text",  nil, monitor1, upperRight, nil, nil},
-        {"Mail",  nil, monitor2, upperRight, nil, nil},
-        {"Microsoft Teams",  nil, monitor2, upperRight, nil, nil},
-        {"Skype for Business",  nil, monitor2, upperRight, nil, nil},
-        {"Franz",  nil, monitor2, upperRight, nil, nil},
-        {"Spotify",  nil, monitor2, lowerRight, nil, nil},
-        {"Todoist",  nil, monitor2, lowerRight, nil, nil},
-        {"iTerm2",  nil, monitor1, lowerRight, nil, nil},
-        {"Calculator",  nil, monitor1, lowerRight, nil, nil},
-    }
-  end
-  if windowTitle ~= nil then
-    windowLayout = getFilteredWindowLayout(windowLayout, windowTitle)
-    hs.timer.doAfter(0.2,  function() hs.layout.apply(windowLayout) end)
-  else
-    hs.layout.apply(windowLayout)
-  end
+    if hs.screen.allScreens()[1]:name() == "PHL BDM4037U" then
+        monitor1 = hs.screen.allScreens()[1]:name()
+        -- maximized window hs.geometry.unitrect({x=1, y=1, w=1, h=1}).
+        windowLayout = {
+            { "Google Chrome", nil, monitor1, hs.geometry.unitrect({ x = 0.5, y = 0, w = 0.25, h = 1 }), nil, nil },
+            { "Firefox", nil, monitor1, hs.geometry.unitrect({ x = 0.5, y = 0, w = 0.25, h = 1 }), nil, nil },
+            { "PyCharm", nil, monitor1, hs.layout.left50, nil, nil },
+            { "WebStorm", nil, monitor1, hs.layout.left50, nil, nil },
+            { "PyCharm", "Commit Changes", monitor1, hs.layout.right50, nil, nil },
+            { "Sublime Text", nil, monitor1, upper50RightRight, nil },
+            { "Mail", nil, monitor1, upper50RightRight, nil, nil },
+            { "Microsoft Teams", nil, monitor1, upper50RightRight, nil, nil },
+            { "Skype for Business", nil, monitor1, upper50RightRight, nil, nil },
+            { "Franz", nil, monitor2, upper50RightRight, nil, nil },
+            { "Spotify", nil, monitor1, lowerRight, nil, nil },
+            { "Todoist", nil, monitor1, lowerRight, nil, nil },
+            { "Iterm2", nil, monitor1, upper50RightRight, nil, nil },
+            { "Calculator", nil, monitor1, lowerRight, nil, nil },
+        }
+    else
+        monitor1 = hs.screen.allScreens()[1]:name()
+        monitor2 = hs.screen.allScreens()[2]:name()
+        -- maximized window hs.geometry.unitrect({x=1, y=1, w=1, h=1}).
+        windowLayout = {
+            { "Google Chrome", nil, monitor2, hs.layout.left50, nil, nil },
+            { "Firefox", nil, monitor2, hs.layout.left50, nil, nil },
+            { "PyCharm", nil, monitor1, hs.layout.left50, nil, nil },
+            { "PyCharm", "Commit Changes", monitor1, hs.layout.right50, nil, nil },
+            { "WebStorm", nil, monitor1, hs.layout.left50, nil, nil },
+            { "Sublime Text", nil, monitor1, upperRight, nil, nil },
+            { "Mail", nil, monitor2, upperRight, nil, nil },
+            { "Microsoft Teams", nil, monitor2, upperRight, nil, nil },
+            { "Skype for Business", nil, monitor2, upperRight, nil, nil },
+            { "Franz", nil, monitor2, upperRight, nil, nil },
+            { "Spotify", nil, monitor2, lowerRight, nil, nil },
+            { "Todoist", nil, monitor2, lowerRight, nil, nil },
+            { "iTerm2", nil, monitor1, lowerRight, nil, nil },
+            { "Calculator", nil, monitor1, lowerRight, nil, nil },
+        }
+    end
+    if windowTitle ~= nil then
+        windowLayout = getFilteredWindowLayout(windowLayout, windowTitle)
+        hs.timer.doAfter(0.2, function()
+            hs.layout.apply(windowLayout)
+        end)
+    else
+        hs.layout.apply(windowLayout)
+    end
 end
 
-hs.hotkey.bind({"cmd", "alt"}, "t", arrangeWindows)
+hs.hotkey.bind({ "cmd", "alt" }, "t", arrangeWindows)
 wifiWatcher = nil
 homeSSID = "Fenix"
 workSSID = "SmithNet"
@@ -158,10 +157,10 @@ end
 wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
 wifiWatcher:start()
 
-hs.hotkey.bind({"cmd"}, "d", function()
-  hs.grid.setGrid('4x2')
-  hs.grid.setMargins('0x0')
-  hs.grid.toggleShow()
+hs.hotkey.bind({ "cmd" }, "d", function()
+    hs.grid.setGrid('4x2')
+    hs.grid.setMargins('0x0')
+    hs.grid.toggleShow()
 end)
 
 isCheatsheetToggled = false
@@ -170,7 +169,7 @@ function toggleCheatsheet()
     isCheatsheetToggled = not isCheatsheetToggled
 end
 
-hs.hotkey.bind({"cmd", "alt"}, "c", function()
+hs.hotkey.bind({ "cmd", "alt" }, "c", function()
     if isCheatsheetToggled then
         spoon.KSheet:hide()
     else
@@ -187,12 +186,11 @@ end
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
 
-
 toggle = false
 storedWindowsLayout = {}
 storedAllVisibleWindows = {}
 
-hs.hotkey.bind(nil, "F19", function ()
+hs.hotkey.bind(nil, "F19", function()
     toggle = not toggle
     carlLogger.df('State of toggle is %s', toggle)
     if toggle then
@@ -200,7 +198,7 @@ hs.hotkey.bind(nil, "F19", function ()
         focusedWindow = hs.window.focusedWindow()
         carlLogger.df('Focused window is %s', focusedWindow)
         hideAllActiveWindows()
-        hs.timer.doAfter(0.01,  function()
+        hs.timer.doAfter(0.01, function()
             focusedWindow:application():unhide()
             focusedWindow:centerOnScreen(hs.mouse:getCurrentScreen())
         end)
@@ -213,7 +211,7 @@ end)
 
 udemyWindow = false
 
-hs.hotkey.bind(nil, "F17", function ()
+hs.hotkey.bind(nil, "F17", function()
     focusedWindow = hs.window.focusedWindow()
     udemyWindow = udemyWindow or hs.window.find('Udemy')
     udemyWindow:application():activate()
@@ -221,7 +219,7 @@ hs.hotkey.bind(nil, "F17", function ()
     focusedWindow:application():activate()
 end)
 
-hs.hotkey.bind(nil, "F16", function ()
+hs.hotkey.bind(nil, "F16", function()
     focusedWindow = hs.window.focusedWindow()
     udemyWindow = udemyWindow or hs.window.find('Udemy')
     udemyWindow:application():activate()
